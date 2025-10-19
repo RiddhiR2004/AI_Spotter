f22fdd0d867ead9985e0008f20b3f0c7da24cee1
RiddhiR2004 <riddhiranjan82@gmail.com>
2025-10-19 20:14:38 +0530

    home screen

--- app/src/main/java/com/nmims/fitness/HomeActivity.java
+++ app/src/main/java/com/nmims/fitness/HomeActivity.java
@@ -8,12 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class WorkoutPlanActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_workout_plan);
+        setContentView(R.layout.activity_home);
+
     }
 }
